5f8c45efa48214fe41c3662f28532ad795bce366
NuiS4ncE <nuis4nce@hotmail.com>
2020-01-15 13:33:10 +0200

    parts 1.1, 1.2, 1.3, 1.4 and 1.5 done. forgot total points. fixed that.

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -35,8 +35,41 @@ const Content = (props) => {
   }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  /* const part1 = 'Fundamentals of React'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+ 
+  return (
+    <div>
+
+      <Header course={course.name} />
+      <Content content={Content}
+      name1={course.parts[0].name} exercises1={course.parts[0].exercises}
+      name2={course.parts[1].name} exercises2={course.parts[1].exercises}
+      name3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
+      <Total points={course.parts[0].exercises + course.parts[1].exercises
+       + course.parts[2].exercises} />
+       </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+/* const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
@@ -54,7 +87,7 @@ const App = () => {
     name: 'State of a component',
     exercises: 14
   } */
-  const parts = [
+  /* const parts = [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -67,17 +100,5 @@ const App = () => {
       name: 'State of a component',
       exercises: 14
     }
-  ]
-  return (
-    <div>
-
-      <Header course={course} />
-      <Content content={Content}
-      name1={parts[0].name} exercises1={parts[0].exercises}
-      name2={parts[1].name} exercises2={parts[1].exercises}
-      name3={parts[2].name} exercises3={parts[2].exercises}/>
-      </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+  ] */
+   /* const course = 'Half Stack application development' */
