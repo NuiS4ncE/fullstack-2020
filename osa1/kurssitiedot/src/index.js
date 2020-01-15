32487497d14e8234ea49022cb2b99f5f030eb9e4
NuiS4ncE <nuis4nce@hotmail.com>
2020-01-16 01:22:19 +0200

    did 1.1 again so it's in the commit

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -5,9 +5,13 @@ import ReactDOM from 'react-dom'
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <div>
+        <div>
+          {props.name}, {props.exercises}
+        </div>
+        {props.name2}, {props.exercises2}
+      </div>
+      {props.name3}, {props.exercises3}
     </div>
   )
 }
@@ -21,46 +25,44 @@ const Header = (props) => {
     </div>
   )
 }
-
+/*
 const Part = (props) => {
   return (
     <p>{props.name}, {props.exercises} </p>
   )
 }
-
+*/
 const Total = (props) => {
   return (
     <div>
-      <p>Total points: {props.parts[0].exercises + props.parts[1].exercises
-      + props.parts[2].exercises}</p>
+      <p>Total exercises: {props.exercises1 + props.exercises2
+        + props.exercises3}</p>
     </div>
   )
 }
 
 const App = () => {
-const course = 'Half Stack application development'
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-] 
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
 
   return (
     <div>
 
-      <Header course={course.name} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header course={course} />
+      <Content content={Content}
+        name={part1} exercises={exercises1}
+        name2={part2} exercises2={exercises2}
+        name3={part3} exercises3={exercises3} />
+      <Total total={Total}
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+      />
     </div>
   )
 }
@@ -103,3 +105,18 @@ ReactDOM.render(<App />, document.getElementById('root'))
       }
     ]
   } */
+/*const course = 'Half Stack application development'
+const parts = [
+{
+  name: 'Fundamentals of React',
+  exercises: 10
+},
+{
+  name: 'Using props to pass data',
+  exercises: 7
+},
+{
+  name: 'State of a component',
+  exercises: 14
+}
+] */
