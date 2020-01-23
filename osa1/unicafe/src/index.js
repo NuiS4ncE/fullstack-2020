20cd0ed567efc15cdb7d8fd77c36c402e6bdeefd
NuiS4ncE <nuis4nce@hotmail.com>
2020-01-23 16:59:09 +0200

    1.7 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,59 +2,72 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = (props) => {
-    const { handleClick, text } = props
-    return (
-        <button onClick={handleClick}>
-            {text}
-        </button>
-    )
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 }
 
 const Header = (props) => {
-    return (
-        <div>
-            <h1>
-                {props.headery}
-            </h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>
+        {props.headery}
+      </h1>
+    </div>
+  )
 }
 
-const App = () => {
-    // tallenna napit omaan tilaansa
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const fbheader = 'give feedback'
-    const statheader = 'statistics'
 
-    return (
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const fbheader = 'give feedback'
+  const statheader = 'statistics'
+  const [average, setAverage] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  return (
+    <div>
+      <div>
         <div>
+          <div>
             <div>
+              <div>
                 <div>
-                    <div>
-                        <Header headery={fbheader} />
-                        <button onClick={() => setGood(good + 1)}>
-                            good
+                  <Header headery={fbheader} />
+                  <button onClick={() => setGood(good + 1)}>
+                    good
             </button>
-                        <button onClick={() => setNeutral(neutral + 1)}>
-                            neutral
+                  <button onClick={() => setNeutral(neutral + 1)}>
+                    neutral
             </button>
-                        <button onClick={() => setBad(bad + 1)}>
-                            bad
+                  <button onClick={() => setBad(bad + 1)}>
+                    bad
             </button>
-                        <Header headery={statheader} />
-                    </div>
-                    good {good}
+                  <Header headery={statheader} />
                 </div>
-                neutral {neutral}
+                good {good}
+              </div>
+              neutral {neutral}
             </div>
             bad {bad}
-            
+          </div>
+          all {(good + bad + neutral)}
         </div>
-    )
+        average {(bad + good + neutral) === 0 ? 0 : (good - bad) / (bad + good + neutral)}
+      </div>
+      positive {(good + bad + neutral) === 0 ? 0 :
+      good/(good + bad + neutral)}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />,
-    document.getElementById('root')
+  document.getElementById('root')
 )
