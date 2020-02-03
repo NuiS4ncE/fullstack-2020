aada68c10f1219b0b4607b709203d21afbae508a
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-03 15:38:03 +0200

    2.10 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [addFilter, setNewFilter] = useState('')
+const Filter = (props) => {
 
   const setFilter = (event) => {
     event.preventDefault()
-    returnFilter(addFilter)
-    setNewFilter('')
+    props.setNewFilter('')
 
   }
 
+  const handleFilterChange = (event) => {
+    props.setNewFilter(event.target.value)
+  }
+
+
+  return (
+    <form onSubmit={setFilter}>
+      <div>
+        filter shown with <input value={props.filter}
+          onChange={handleFilterChange} />
+      </div>
+    </form>
+  )
+}
+
+const PersonForm = (props) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
   const addNameNumber = (event) => {
     event.preventDefault()
-    if (persons.map((person) => person.name).includes(newName)) {
+    if (props.persons.map((person) => person.name).includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       const nameObject = {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(nameObject))
+      props.setPersons(props.persons.concat(nameObject))
       setNewName('')
       setNewNumber('')
     }
@@ -39,15 +48,29 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  return (
+    <form onSubmit={addNameNumber}>
+      <div>
+        name: <input value={newName}
+          onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber}
+          onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
 
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-  }
+const Persons = (props) => {
 
   const returnFilter = () => {
-    const setFiltertoShow = persons.filter(person =>
-        person.name.toLowerCase().indexOf(addFilter.toLowerCase()) !== -1)
-    if (addFilter !== '') {
+    const setFiltertoShow = props.persons.filter(person =>
+      person.name.toLowerCase().indexOf(props.filter.toLowerCase()) !== -1)
+    if (props.filter !== '') {
       return (
         setFiltertoShow.map((person) =>
           <p key={person.name}>
@@ -56,39 +79,38 @@ const App = () => {
         ))
     } else {
       return (
-        persons.map((person) =>
+        props.persons.map((person) =>
           <p key={person.name}>
             {person.name} {person.number}
           </p>
         ))
     }
   }
+  return (
+    <div>
+      {returnFilter()}
+    </div>
+  )
+
+}
+
+const App = () => {
+  const [addFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={setFilter}>
-        <div>
-          filter shown with <input value={addFilter}
-            onChange={handleFilterChange} />
-        </div>
-      </form>
-      <h2>add a new</h2>
-      <form onSubmit={addNameNumber}>
-        <div>
-          name: <input value={newName}
-            onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber}
-            onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {returnFilter()}
+      <Filter filter={addFilter} setNewFilter={setNewFilter} />
+      <h3>add a new</h3>
+      <PersonForm persons={persons} setPersons={setPersons} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={addFilter} />
     </div>
   )
 
