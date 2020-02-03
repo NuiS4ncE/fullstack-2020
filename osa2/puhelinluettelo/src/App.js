afccd4bed13188a24b185430feadf39d8f433b4d
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-03 13:42:50 +0200

    2.9 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1231244'
-    }
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [addFilter, setNewFilter] = useState('')
+
+  const setFilter = (event) => {
+    event.preventDefault()
+    returnFilter(addFilter)
+    setNewFilter('')
+
+  }
 
   const addNameNumber = (event) => {
     event.preventDefault()
@@ -17,7 +25,7 @@ const App = () => {
     } else {
       const nameObject = {
         name: newName,
-        number : newNumber
+        number: newNumber
       }
       setPersons(persons.concat(nameObject))
       setNewName('')
@@ -32,9 +40,40 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const returnFilter = () => {
+    const setFiltertoShow = persons.filter(person =>
+        person.name.toLowerCase().indexOf(addFilter.toLowerCase()) !== -1)
+    if (addFilter !== '') {
+      return (
+        setFiltertoShow.map((person) =>
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))
+    } else {
+      return (
+        persons.map((person) =>
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={setFilter}>
+        <div>
+          filter shown with <input value={addFilter}
+            onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addNameNumber}>
         <div>
           name: <input value={newName}
@@ -49,11 +88,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) =>
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      )}
+      {returnFilter()}
     </div>
   )
 
