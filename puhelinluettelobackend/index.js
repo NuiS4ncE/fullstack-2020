e0a35cee4db9cb433de98c1fdf844590b5c16caf
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-07 23:12:01 +0200

    3.5 done

--- puhelinluettelobackend/index.js
+++ puhelinluettelobackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         name: "Arto Hellas",
@@ -44,6 +46,33 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+}) 
+
+const generateId = () => {
+    const maxId = persons.length > 0 
+    ? Math.max(...persons.map(n => n.id))
+    : 0 
+    return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    
+    if(!body.content){
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+    
+    const person = {
+        name: body.name, 
+        number: body.number, 
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
