60044049696322f06ae9b64f5595565181cd406e
NuiS4ncE <nuis4nce@hotmail.com>
2020-03-02 03:15:33 +0200

    6.5 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -20,20 +20,30 @@ const App = () => {
     event.target.anecdote.value = ''
   }
 
+  const anecdoteSort = () => {
+    const anecdotties = anecdotes.sort((a,b) => b.votes - a.votes)
+    const mappedAnecdotes = anecdotties.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )  
+    return ( 
+      mappedAnecdotes
+    )
+}
+
+
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      {anecdoteSort()}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div><input name="anecdote"/></div>
